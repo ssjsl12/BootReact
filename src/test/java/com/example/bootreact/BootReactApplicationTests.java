bd4865984e7a2ede54ba5a4593a08bbe53b0182a
leeminjong <ssjsl12@naver.com>
2025-01-16 16:00:27 +0900

    [민종]  - 초기 설정

---
+++ src/test/java/com/example/bootreact/BootReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bootreact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootReactApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
